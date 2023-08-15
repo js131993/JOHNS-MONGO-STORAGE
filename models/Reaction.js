39aed8a7e506f21761a7e3a1e0a95d8b263913e0
js131993 <john.sanders4444@gmail.com>
2023-08-15 16:50:24 -0400

    Update Reaction.js
    updating reaction file, no virtuals needed.

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,1 +1,32 @@
 const { Schema, Types } = require("mongoose");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      get: (date) => date.toLocaleDateString(),
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
