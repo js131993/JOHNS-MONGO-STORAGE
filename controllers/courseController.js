85eda916f8412af6259e4d908f443d921ae9e161
js131993 <john.sanders4444@gmail.com>
2023-08-21 15:57:25 -0400

    Update courseController.js

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -27,3 +27,28 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+    // Create a course
+  async createCourse(req, res) {
+    try {
+      const course = await Course.create(req.body);
+      res.json(course);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // Delete a course
+  async deleteCourse(req, res) {
+    try {
+      const course = await Course.findOneAndDelete({ _id: req.params.courseId });
+
+      if (!course) {
+        res.status(404).json({ message: 'No course with that ID' });
+      }
+
+      await Student.deleteMany({ _id: { $in: course.students } });
+      res.json({ message: 'Course and students deleted!' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
